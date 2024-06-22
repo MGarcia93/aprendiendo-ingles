d1b1f7f134d10fcf03c2078fffabec8235c1cb19
maximiliano garcia <maximiliano_garcia93@hotmail.com>
2024-06-22 18:37:10 -0300

    add icons

---
+++ src/components/React/icons/Verify.jsx
@@ -0,0 +1,16 @@
+
+export const Verify = () =>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="w-5 h-5 text-foreground-900"
+    >
+        <path d="M20 6 9 17l-5-5"></path>
+    </svg>
