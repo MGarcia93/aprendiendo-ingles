78155fec4945c124337db837e74e9eb067958500
maximiliano garcia <maximiliano_garcia93@hotmail.com>
2024-06-22 19:59:04 -0300

    add const y cambio a typescript

--- src/data/constant.js
+++
@@ -1,1 +0,0 @@
-export const TOTAL_VERBS=10;
---
+++ src/data/constant.ts
@@ -0,0 +1,23 @@
+import type { controlVerb } from "../types/types";
+
+export const TOTAL_VERBS=10;
+
+export const controls: controlVerb[] = [
+    {
+        label: 'Infinitivo',
+        name: 'infinitive'
+    },
+    {
+        label: 'Presente Continuo',
+        name: 'presentContinuous'
+    },
+    {
+        label: 'Pasado Simple',
+        name: 'pastSimple'
+    },
+    {
+        label: 'Futuro',
+        name: 'future'
+    },
+
+]
