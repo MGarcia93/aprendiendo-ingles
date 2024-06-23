8b3e38b4b181ed75a5937e3aa71889b7ecebd376
maximiliano garcia <maximiliano_garcia93@hotmail.com>
2024-06-23 17:20:01 -0300

    add className prop

--- src/components/React/form/Button.tsx
+++ src/components/React/form/Button.tsx
@@ -1,30 +1,18 @@
-import type { stageVerb } from "../../../types/types";
+import type { buttons, stageVerb } from "../../../types/types";
 import { ArrowNext } from "../icons/ArrowNext";
 import { Verify } from "../icons/Verify";
 
 interface Props {
     stage: stageVerb,
-    onClick: () => void
+    onClick: () => void,
+    buttons: buttons
 }
 
-const buttons = {
-    verify: {
-        label: "Verficar",
-        icon: Verify,
-    },
-    next: {
-        label: "Siguiente Verbo",
-        icon: ArrowNext,
-    },
-    finish: {
-        label: "Finalizar",
-        icon: Verify,
-    }
-}
-export function Button({ stage, onClick }: Props) {
-    const { label, icon: Icon } = buttons[stage];
+
+export function Button({ stage, onClick, buttons }: Props) {
+    const { label, icon: Icon, className } = buttons[stage];
     return <button
-        className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2"
+        className={`inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent  hover:text-accent-foreground h-10 px-4 py-2 ${className ?? ''}`}
         onClick={onClick}
     >
         <Icon></Icon>
