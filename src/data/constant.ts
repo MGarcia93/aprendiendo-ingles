3b31c7421e41b01c6e410ed522391ffe6ec6465a
maximiliano garcia <maximiliano_garcia93@hotmail.com>
2024-06-23 17:20:16 -0300

    create buttons

--- src/data/constant.ts
+++ src/data/constant.ts
@@ -1,6 +1,10 @@
-import type { controlVerb } from "../types/types";
+import { ArrowBack } from "../components/React/icons/ArrowBack";
+import { ArrowNext } from "../components/React/icons/ArrowNext";
+import { Verify } from "../components/React/icons/Verify";
+import type { buttonInterface, controlVerb, stageVerb } from "../types/types";
 
 export const TOTAL_VERBS=10;
+export const TOTAL_WORDS=10;
 
 export const controls: controlVerb[] = [
     {
@@ -21,3 +25,35 @@ export const controls: controlVerb[] = [
     },
 
 ]
+
+
+export const buttonsVerb:Record<stageVerb,buttonInterface> = {
+    verify: {
+        label: "Verficar",
+        icon: Verify,
+
+    },
+    next: {
+        label: "Siguiente Verbo",
+        icon: ArrowNext,
+    },
+    finish: {
+        label: "Finalizar",
+        icon: ArrowBack,
+        className:"bg-zinc-900 text-white"
+    }
+}
+export const buttonsWord:Record<stageVerb,buttonInterface> = {
+    verify: {
+        label: "Verificar",
+        icon: Verify,
+    },
+    next: {
+        label: "Siguiente Palabra",
+        icon: ArrowNext,
+    },
+    finish: {
+        label: "Finalizar",
+        icon: ArrowBack,
+    }
+}
