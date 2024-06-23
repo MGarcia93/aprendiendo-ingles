d4d832330359aa3ce0a7e31261972eb2944b16f2
maximiliano garcia <maximiliano_garcia93@hotmail.com>
2024-06-23 16:58:31 -0300

    add button speaker

--- src/components/React/Example.tsx
+++ src/components/React/Example.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import type { idioma, ejemplo } from "../../types/types";
 import { Translate } from "./icons/Translate";
+import { Mic } from "./icons/Mic";
+import api from "../../utils/api";
 
 interface Props {
     example: ejemplo
@@ -12,6 +14,8 @@ export function Example({ example }: Props) {
     }
     return <li className="flex gap-2 items-center">
          <button title="traducir" onClick={handleChageIdioma}><Translate className='w-5 h-5'></Translate></button>
+        <button onClick={() => api.speakWord(example.ing)}><Mic className="w-5 h-5" /></button>
+
         {example[idioma]}
     </li>
 }
