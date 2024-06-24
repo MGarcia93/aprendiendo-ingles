51c648cc67235a85e7a41f45437665f0c94235cc
maximiliano garcia <maximiliano_garcia93@hotmail.com>
2024-06-23 21:32:42 -0300

    change icon in not required

--- src/components/React/form/Button.tsx
+++ src/components/React/form/Button.tsx
@@ -12,10 +12,10 @@ interface Props {
 export function Button({ stage, onClick, buttons }: Props) {
     const { label, icon: Icon, className } = buttons[stage];
     return <button
-        className={`inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent  hover:text-accent-foreground h-10 px-4 py-2 ${className ?? ''}`}
+        className={`inline-flex items-center  justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent  hover:text-accent-foreground h-10 px-4 py-2 ${className ?? ''}`}
         onClick={onClick}
     >
-        <Icon></Icon>
+       {Icon && <Icon></Icon>}
         {label}
     </button>
 
