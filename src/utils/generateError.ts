314e7b327cc79b9497e6028a834558ff44605e54
maximiliano garcia <maximiliano_garcia93@hotmail.com>
2024-06-24 21:54:26 -0300

    change get  shuffle with 70%

--- src/utils/generateError.ts
+++ src/utils/generateError.ts
@@ -1,9 +1,9 @@
-const typeError:Record<string,(pharse:string)=>string> = {
-    none: (pharse: string):string => pharse,
+const typeError: Record<string, (pharse: string) => string> = {
+    none: (pharse: string): string => pharse,
     changeToBe: changeToBe,
-    shuffleWords:shuffleWords,
-    removeAux:removeAux,
-    changeWH:changeWH
+    shuffleWords: shuffleWords,
+    removeAux: removeAux,
+    changeWH: changeWH
 }
 
 function changeToBe(pharse: string) {
@@ -27,43 +27,48 @@ function changeToBe(pharse: string) {
     });
 }
 function changeWH(pharse: string) {
-    const words= ["What", "Why", "Where", "Who", "When", "How"];
+    const words = ["What", "Why", "Where", "Who", "When", "How"];
     return pharse.replace(/\b(what|why|where|who|when|how)\b/gi, function (match) {
-        const remplaces=words.filter((w)=>w!=match);
-        return remplaces[Math.floor(Math.random() * remplaces.length)] ;
-       
+        const remplaces = words.filter((w) => w != match);
+        return remplaces[Math.floor(Math.random() * remplaces.length)];
+
     });
 }
 
-function shuffleWords(pharse:string):string {
+function shuffleWords(pharse: string): string {
     let words = pharse.split(' ');
     let lastWord = words.pop() as string;
     let swaps = 0;
     for (let i = 0; i < Math.min(words.length - 1, 2); i++) {
-      const j = Math.floor(Math.random() * (words.length - 1)) + 1;
-      [words[i], words[j]] = [words[j], words[i]];
-      swaps++;
+        const j = Math.floor(Math.random() * (words.length - 1)) + 1;
+        [words[i], words[j]] = [words[j].toLowerCase(), words[i].toLowerCase()];
+        swaps++;
     }
-    if (swaps > 0) {
-      let firstWord = words[0];
-      words[0] = firstWord.toLowerCase();
-      words[0] = words[0].charAt(0).toUpperCase() + words[0].slice(1);
-    }
+
+    let firstWord = words[0];
+    words[0] = firstWord.toLowerCase();
+    words[0] = words[0].charAt(0).toUpperCase() + words[0].slice(1);
     words.push(lastWord);
     let shuffledPharse = words.join(' ');
     return shuffledPharse;
-  }
+}
 
-  function removeAux(pharse:string) {
+function removeAux(pharse: string) {
     // Expresión regular para identificar los verbos auxiliares en presente y pasado
     const regex = /^(am|is|are|was|were|have|has|had|do|does|did)\b\s*/i;
-    
+
     // Reemplazar los verbos auxiliares por una cadena vacía
     return pharse.replace(regex, '');
-  }
+}
 export function generateError(pharse: string) {
-    const errores=Object.keys(typeError);
-    const error=errores[Math.floor(Math.random() * errores.length)]
-    console.log({error})
+    const errores = Object.keys(typeError);
+    let error: string
+    const randomNumber = Math.floor(Math.random() * 10);
+    if (randomNumber < 7) {
+        error = 'shuffleWords';
+    } else {
+        const filteredErrores = errores.filter(err => err !== 'shuffleWords');
+        error = filteredErrores[Math.floor(Math.random() * filteredErrores.length)];
+    }
     return typeError[error](pharse);
 }
